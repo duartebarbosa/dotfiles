beb6d1a2415d7b623ad8164b8b5b729daa254f2d
Duarte Barbosa <duarte.barbosa@gmail.com>
2013-02-19 18:49:25 +0000

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+vi /etc/apt/sources.list
+apt-get update
+apt-get dist-upgrade
+apt-get install zsh git clementine vlc vlc-mozilla fglrx
+git config --global user.name "Duarte Barbosa"
+git config --global user.email duarte.barbosa@gmail.com
+git config --global core.excludesfile ~/.gitignore_global
